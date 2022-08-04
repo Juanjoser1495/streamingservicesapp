f895cff69917317548cf9b38db05f0d9145f2c32
juan.rojas <juanjoserojasv@gmail.com>
2022-08-04 09:38:43 -0500

    Adding the app and the navbar and footer

---
+++ src/StreamingApp.js
@@ -0,0 +1,15 @@
+import StreamingFooter from "./components/StreamingFooter";
+import StreamingGrid from "./components/StreamingGrid";
+import StreamingNavbar from "./components/StreamingNavbar";
+
+const StreamingApp = () => {
+  return (
+    <>
+      <StreamingNavbar></StreamingNavbar>
+      <StreamingGrid></StreamingGrid>
+      <StreamingFooter></StreamingFooter>
+    </>
+  );
+};
+
+export default StreamingApp;
