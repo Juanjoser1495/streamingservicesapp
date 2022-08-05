2becefb884234375849c19ba97f3a271f14a6cb9
juan.rojas <juanjoserojasv@gmail.com>
2022-08-05 15:05:34 -0500

    Giving behaviour to the page and adding the search category component

--- src/StreamingApp.js
+++ src/StreamingApp.js
@@ -1,12 +1,17 @@
 import StreamingFooter from "./components/StreamingFooter";
 import StreamingGrid from "./components/StreamingGrid";
 import StreamingNavbar from "./components/StreamingNavbar";
+import StreamingSearchCategory from "./components/StremingSearchCategory";
+import { useState } from "react";
 
 const StreamingApp = () => {
+  const [category, setCategory] = useState("action");
+
   return (
     <>
       <StreamingNavbar></StreamingNavbar>
-      <StreamingGrid></StreamingGrid>
+      <StreamingSearchCategory setCategory={setCategory}></StreamingSearchCategory>
+      <StreamingGrid category={category}></StreamingGrid>
       <StreamingFooter></StreamingFooter>
     </>
   );
--- src/components/StreamingGrid.js
+++ src/components/StreamingGrid.js
@@ -1,18 +1,24 @@
 import StreamingGridItem from "./StreamingGridItem";
 import { useFetchRankingTop10 } from "../hooks/useFetchRankingTop10";
 
-const StreamingGrid = () => {
-  const { ranking, message } = useFetchRankingTop10("action");
-  console.log(ranking);
+const StreamingGrid = ({ category }) => {
+  const { ranking, message } = useFetchRankingTop10(category);
+
+  const isRankingAvailable = message == "Data successufull retrieved" ? true : false;
+
   return (
     <>
-      {ranking.map((data) => (
-        <StreamingGridItem
-          key={data.id}
-          url={data.imageUrl}
-          title={data.movieName}
-        ></StreamingGridItem>
-      ))}
+      {isRankingAvailable ? (
+        ranking.map((data) => (
+          <StreamingGridItem
+            key={data.id}
+            url={data.imageUrl}
+            title={data.movieName}
+          ></StreamingGridItem>
+        ))
+      ) : (
+        <p>{message}</p>
+      )}
     </>
   );
 };
