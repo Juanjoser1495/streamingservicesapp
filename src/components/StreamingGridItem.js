f57d90a3e9e369df7bb58d9e48090d4a15921423
juan.rojas <juanjoserojasv@gmail.com>
2022-08-09 12:12:06 -0500

    Consuming the endpoint from the component

--- src/components/StreamingGridItem.js
+++ src/components/StreamingGridItem.js
@@ -1,21 +1,28 @@
 import "../css/StreamingGridItem.css";
+import { saveLike } from "../utils/saveLike";
 
 const StreamingGridItem = ({ url, title }) => {
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    saveLike(title);
+  };
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm centerContent">
-            <img className="maxWidth maxHeight" src={url} alt={title}></img>
-          </div>
-          <div className="col-sm centerContent">
-            <p>{title}</p>
-          </div>
-          <div className="col-sm centerContent">
-            <button>Test2</button>
+      <form onSubmit={handleOnSubmit}>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm centerContent">
+              <img className="maxWidth maxHeight" src={url} alt={title}></img>
+            </div>
+            <div className="col-sm centerContent">
+              <p>{title}</p>
+            </div>
+            <div className="col-sm centerContent">
+              <button type="submit">Like</button>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </>
   );
 };
