2a362390f59b79be30da96bc738caf454dbf305e
Juan Jose Rojas Velez <juanjoserojasv@gmail.com>
2023-05-01 16:20:57 -0400

    Handling the answer of the new service

--- src/components/StreamingGenresRank.js
+++ src/components/StreamingGenresRank.js
@@ -1,14 +1,23 @@
 import { getGenreRanking } from "../utils/getGenreRanking";
+import { useState } from "react";
 
 const StreamingGenresRank = () => {
-  const { status, ranking, httpStatus } = getGenreRanking();
-  console.log(ranking);
+  getGenreRanking().then(({ ranking, status, httpStatus }) => {
+    setRankingGenres(ranking);
+  });
+
+  const [rankingGenres, setRankingGenres] = useState({ ranking: [] });
+  //console.log(ranking);
+
+  if (rankingGenres === undefined) {
+    return <>Still loading...</>;
+  }
+
   return (
     <>
       <div className="d-flex align-items-center justify-content-center vh-100">
-        <p className="lead">{JSON.stringify(ranking)}</p>
-        <p className="lead">{ranking}</p>
-        <p className="lead">{status}</p>
+        <p className="lead">{JSON.stringify(rankingGenres)}</p>
+
         <p className="lead">Lo que sea</p>
       </div>
     </>
